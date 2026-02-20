bb8c494839441655b93857bcb380296146bd81f4
armadne <madane.aroul@hotmail.com>
2026-02-20 20:06:10 +0100

    "#"

--- vite-config.js
+++ vite-config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'armadne.github.io',  // nom exact du repo
+  base: 'armadne.github.io', 
 })
